0efe3723a35122fe6029a2510b3ca9750eeec740
Niko Auer <nikoauer26@gmail.com>
2023-12-06 10:56:34 +1100

    modified get shoe function to take req params

--- server/controllers/shoeControllers.js
+++ server/controllers/shoeControllers.js
@@ -3,10 +3,18 @@ const { Shoe } = require("../models");
 module.exports = {
   async getShoes(req, res) {
     try {
-      const shoes = await Shoe.find();
-      res.json(shoes);
+      const soleCmValue = req.body.soleCm;
+      const foundShoes = await Shoe.findOne({ soleCm: soleCmValue});
+      if (foundShoes) {
+        console.log('Found shoe:', foundShoes);
+        res.status(200).json(foundShoes);
+      } else {
+        console.log('Shoe not found for size', soleCmValue);
+        res.status(404).json({message: 'Shoe not found'});
+      }
     } catch (error) {
-      res.status(500).json(error);
+      console.error('Error finding shoe:', error);
+      res.status(500).json({ error: 'Internal server error' });
     }
   },
 };
