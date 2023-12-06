0fd635940755cdd4939aa27e3ff43ed5ebd6d654
Niko Auer <nikoauer26@gmail.com>
2023-12-06 14:54:01 +1100

    reversed get shoe function to just using soleCM

--- server/controllers/shoeControllers.js
+++ server/controllers/shoeControllers.js
@@ -3,22 +3,18 @@ const { Shoe } = require("../models");
 module.exports = {
   async getShoes(req, res) {
     try {
-      //retrieve req data
-      const {soleCm, toeBox, Width} = req.body;
+      const { soleCM } = req.body;
 
-      //add req body data into a variable
-      const searchCriteria = { soleCm, toeBox, Width };
+      const searchCriteria = { soleCM };
 
-      // delete any undefined properties in the req data 
-      Object.keys(searchCriteria).forEach(key => searchCriteria[key] === undefined && delete searchCriteria[key]);
+      const foundShoes = await Shoe.find(searchCriteria);
 
-      const foundShoes = await Shoe.findOne(searchCriteria);
       if (foundShoes) {
         console.log('Found shoe:', foundShoes);
         res.status(200).json(foundShoes);
       } else {
-        console.log('Shoe not found for size');
-        res.status(404).json({message: 'Shoe not found'});
+        console.log('Shoe not found for soleCm', soleCM);
+        res.status(404).json({ message: 'Shoe not found' });
       }
     } catch (error) {
       console.error('Error finding shoe:', error);
