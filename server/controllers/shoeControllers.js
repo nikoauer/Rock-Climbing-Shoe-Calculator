91abfb206aef9ed23283339f1dfda6a55b7cbae9
Niko Auer <nikoauer26@gmail.com>
2023-12-06 11:12:27 +1100

    modified backend getShoe function to take 3 params

--- server/controllers/shoeControllers.js
+++ server/controllers/shoeControllers.js
@@ -3,13 +3,22 @@ const { Shoe } = require("../models");
 module.exports = {
   async getShoes(req, res) {
     try {
-      const soleCmValue = req.body.soleCm;
-      const foundShoes = await Shoe.findOne({ soleCm: soleCmValue});
+      //retrieve req data
+      const {soleCm, toeBox, width} = req.body;
+
+      //add req body data into a variable
+      const searchCriteria = { soleCm, toeBox, width };
+
+      // delete any undefined properties in the req data 
+      Object.keys(searchCriteria).forEach(key => searchCriteria[key] === undefined && delete searchCriteria[key]);
+
+
+      const foundShoes = await Shoe.findOne(searchCriteria);
       if (foundShoes) {
         console.log('Found shoe:', foundShoes);
         res.status(200).json(foundShoes);
       } else {
-        console.log('Shoe not found for size', soleCmValue);
+        console.log('Shoe not found for size');
         res.status(404).json({message: 'Shoe not found'});
       }
     } catch (error) {
