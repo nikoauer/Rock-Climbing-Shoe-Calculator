8d12f626b4b1d77d4275ad19644a013b5c985339
Niko Auer <nikoauer26@gmail.com>
2023-12-08 10:47:26 +1100

    basic form structure built in react

--- client/src/components/ShoeSearch.jsx
+++ client/src/components/ShoeSearch.jsx
@@ -1,6 +1,51 @@
+import { useState } from "react"
 
 export default function ShoeSearch () {
+    const [footData, setFootData] = useState({
+        leftFoot: "",
+        rightFoot: ""
+    })
+
+    const [shoeData, setShoeData] = useState({
+        soleCM: ""
+    })
+
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFootData((prevFootData) => ({
+          ...prevFootData,
+          [name]: value,
+        }));
+      };
+
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        if (footData.leftFoot && footData.rightFoot) {
+            console.log(footData);
+        } else {
+            console.log("Please fill both input fields");
+        }
+      };
+
+
     return (
-        <h1> Hello World</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                type="number"
+                name="leftFoot"
+                value={footData.leftFoot} 
+                onChange={handleChange}
+                />
+                <input 
+                type="number"
+                name="rightFoot"
+                value={footData.rightFoot} 
+                onChange={handleChange}
+                />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
     )
 }
