76f13181126434cec01125df175c2e596400880d
Niko Auer <nikoauer26@gmail.com>
2023-12-09 13:20:52 +1100

    Changed handleSubmit function to use useEffect

--- client/src/components/ShoeSearch.jsx
+++ client/src/components/ShoeSearch.jsx
@@ -18,59 +18,61 @@ export default function ShoeSearch() {
    }));
  };
 
- useEffect(() => {
-   console.log(shoeResults);
- }, [shoeResults]);
-
  const handleSubmit = async (event) => {
    event.preventDefault();
    if (footData.leftFoot && footData.rightFoot) {
-
      setFootData((pastData) => ({
        ...pastData,
        soleCM:
          (parseFloat(footData.leftFoot) + parseFloat(footData.rightFoot)) / 2,
      }));
-     try {
-       const data = { soleCM: footData.soleCM };
-       const response = await sendSize(data);
-       if(!response.ok) {
-           throw new Error('Something went wrong!');
-       } 
-       const shoeInfo = await response.json();
-       setShoeResults(shoeInfo);
-     } catch (error) {
-       console.error(error)
-     }
    }
-  };
+ };
+
+ useEffect(() => {
+   if (footData.leftFoot && footData.rightFoot && footData.soleCM) {
+     (async () => {
+       try {
+         const data = { soleCM: footData.soleCM };
+         const response = await sendSize(data);
+         const shoeInfo = await response.json();
+         if (!response.ok) {
+           throw new Error("Something went wrong!");
+         }
+         setShoeResults(shoeInfo);
+       } catch (error) {
+         console.error(error);
+       }
+     })();
+   }
+ }, [footData.leftFoot, footData.rightFoot, footData.soleCM]);
 
  return (
    <div>
-   <div>
-     <form onSubmit={handleSubmit}>
-       <input
-         type="number"
-         name="leftFoot"
-         value={footData.leftFoot}
-         onChange={handleChange}
-       />
-       <input
-         type="number"
-         name="rightFoot"
-         value={footData.rightFoot}
-         onChange={handleChange}
-       />
-       <button type="submit">Submit</button>
-     </form>
-   </div>
-   {shoeResults.map(shoe => (
-    <div key={shoe._id}>
-      <h2>{shoe.Name}</h2>
-      <p>{shoe.Model}</p>
-      <img src={shoe.imageUrl} alt={shoe.Name} />
-    </div>
-  ))}
+     <div>
+       <form onSubmit={handleSubmit}>
+         <input
+           type="number"
+           name="leftFoot"
+           value={footData.leftFoot}
+           onChange={handleChange}
+         />
+         <input
+           type="number"
+           name="rightFoot"
+           value={footData.rightFoot}
+           onChange={handleChange}
+         />
+         <button type="submit">Submit</button>
+       </form>
+     </div>
+     {shoeResults.length > 0 && shoeResults.map(shoe => (
+       <div key={shoe._id}>
+         <h2>{shoe.Name}</h2>
+         <p>{shoe.Model}</p>
+         <img src={shoe.imageUrl} alt={shoe.Name} />
+       </div>
+     ))}
    </div>
  );
 }
